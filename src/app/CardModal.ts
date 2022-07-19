cb5ec95aa65f1b1820bb8c7762efd49799e0402a
JiRad71 <kirilllkorolev@gmail.com>
2022-07-19 20:36:44 +0300

    feat: fix render cards

--- src/app/CardModal.ts
+++ src/app/CardModal.ts
@@ -3,11 +3,11 @@ import data from "../asset/resource/data.json"
 interface IData {
     num: string,
     name: string,
-    quantity: string,
+    quantity: number,
     cooler:string,
     shape: string,
-    strength: string,
-    size: string,
+    strength: number,
+    size: number,
     favorite: boolean
 }
 
@@ -16,9 +16,8 @@ interface IData {
 type IDates = Record<string, IData>
 
 export class DataModel{
-    dataUrl!: Array<IData>;
+    dataUrl: Array<IData>=[];
     constructor(){
-
     }
 
     public async buildData(){
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import Control from "../common/control";
 import {StorePage} from "./storePage";
 import "../styles/style.css"
 import 'nouislider/dist/nouislider.css';
+import {DataModel} from './CardModal'
 
 export class Application extends Control{
     constructor (parrentNode: HTMLElement){
         super (parrentNode);
-        const storePage = new StorePage(this.node);
+        const model = new DataModel()
+        const storePage = new StorePage(this.node, model);
     }
 }
